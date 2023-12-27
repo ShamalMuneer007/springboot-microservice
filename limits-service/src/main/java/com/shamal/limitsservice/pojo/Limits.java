c593b317cf9881c45b508aa138fd4444b279db12
Shamal Muneer <shamal20017@gmail.com>
2023-12-27 11:21:43 +0530

    First message

---
+++ limits-service/src/main/java/com/shamal/limitsservice/pojo/Limits.java
@@ -0,0 +1,15 @@
+package com.shamal.limitsservice.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Limits {
+    int minimum;
+    int maximum;
+}
